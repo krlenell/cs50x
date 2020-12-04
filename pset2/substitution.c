bd23377854d2405a728698d95dc7c51308310e87
Kevin Lenell <krlenell@gmail.com>
2020-12-03 23:08:06 -0800

    finished substitution.c

--- pset2/substitution.c
+++ pset2/substitution.c
@@ -6,6 +6,9 @@
 const int KEYLENGTH = 26;
 
 
+int make_key(int *key, string);
+
+
 int main(int argc, string argv[])
 {
     //if there is no second argument, or is a third argument, shut it down
@@ -15,16 +18,37 @@ int main(int argc, string argv[])
         return 1;
     }
     string input = argv[1];
-    printf("input: %s\n", input);
     //if the input is not 26 char, shut it all down
     if (strlen(input) != KEYLENGTH)
     {
         printf("Error: Input is not 26 characters\n");
         return 1;
     }
-    char test = 'A' - ('A' - (int) 'F');
-    printf("test: %c\n", test);
     int key[KEYLENGTH];
+
+    if (make_key(key, input))
+    {
+        return 1;
+    }
+
+    string plaintext = get_string("plaintext: ");
+    int length = strlen(plaintext);
+    char result[length];
+    for (int i = 0; i < length; i++)
+    {
+        if (isalpha(plaintext[i]))
+        {
+            char replace = plaintext[i] - key[(int) toupper(plaintext[i]) - 65];
+            plaintext[i] = replace;
+        }
+    }
+
+    printf("ciphertext: %s\n", plaintext);
+}
+
+int make_key(int *key, string input)
+{
+    int check[26] = {0};
     //parse the key into an array with each index being a difference between key and original character
     for (int i = 0; i < KEYLENGTH; i++)
     {
@@ -35,5 +59,14 @@ int main(int argc, string argv[])
             return 1;
         }
         key[i] = (i + 65) - toupper(input[i]);
+        int check_index = toupper(input[i]) - 65;
+        check[check_index] += 1;
+        if (check[check_index] > 1)
+        {
+            printf("Error: repeated characters in key\n");
+            return 1;
+        }
     }
+    //im too lazy for proper comments, sorry
+    return 0;
 }
